d373f9c91229af03304a388d56fcf2f160dea254
Ayan <help.cscord@gmail.com>
2025-02-18 18:10:39 +0000

    Added base for KNA.dll.
    KNA 1.0 is 25% ready.

---
+++ KNA-Main/Class1.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.SqlServer.Server;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Xml.Linq;
+
+namespace KNA
+{
+    public class LDBbuilder
+    {
+        public void newDIR(string name, string path)
+        {
+            if (path == "CurrentDir")
+            {
+                Directory.CreateDirectory(Environment.CurrentDirectory + @"\" + name);
+            }
+            else
+            {
+                Directory.CreateDirectory(path + @"\" + name);
+            }
+        }
+        public void CreateNewSubDIR(string name, string path)
+        {
+            if (path == "CurrentDir")
+            {
+                Directory.CreateDirectory(Environment.CurrentDirectory + @"\" + name);
+            }
+            else
+            {
+                Directory.CreateDirectory(path + @"\" + name);
+            }
+        }
+
+        public void addNewValueToDIR(string valueName, string path, string value)
+        {
+            if (path == "CurrentDir")
+            {
+                File.WriteAllText(Environment.CurrentDirectory + @"\" + valueName, value);
+            }
+            else
+            {
+                File.WriteAllText(path + @"\" + valueName, value);
+            }
+        } 
+    }
+    
+}
